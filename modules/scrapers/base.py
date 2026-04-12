783eb7e162bffe094847da0e751a3e4c2908168c
Julio Morales <julmoralesb@gmail.com>
2026-04-11 22:10:20 -0600

    Created new scrapper abstract class

---
+++ modules/scrapers/base.py
@@ -0,0 +1,25 @@
+"""Base scraper class defining the interface for game scrapers."""
+
+from abc import ABC, abstractmethod
+from modules.models import FreeGame
+
+
+class BaseScraper(ABC):
+    """Abstract base class for game store scrapers."""
+
+    @property
+    @abstractmethod
+    def store_name(self) -> str:
+        """Name of the game store (e.g., 'epic', 'steam')."""
+        pass
+
+    @abstractmethod
+    def fetch_free_games(self) -> list[FreeGame]:
+        """Fetch free games from the store.
+        
+        Returns
+        -------
+        list[FreeGame]
+            List of free game promotions available from the store.
+        """
+        pass
